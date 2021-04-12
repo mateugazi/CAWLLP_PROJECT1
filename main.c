262235285a6c4b491fbd9f5906327042fcfd3f49
cichy30002 <cichy30002@gmail.com>
2021-04-13 00:14:40 +0200

    ini parsing working properly, grade 3.0 requirements accomplished

--- main.c
+++ main.c
@@ -1,36 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
     FILE *iniFile = fopen(argv[1],"r");
-    char slowo[30];
+    char slowo[100];
     char task [62];
     strcpy(task,argv[2]);
+
+    char delim [10] = ".";
+    char *ptr = strtok(task,delim);
+    char taskSection[30];
+    char tmp[32] = "[";
+    strcat(tmp,ptr);
+    strcpy(taskSection,tmp);
+    strcat(taskSection,"]\n");
+
+    ptr = strtok(NULL,delim);
+    char taskKey[30];
+    strcpy(taskKey,ptr);
+
+    //printf("task : %s %s",taskSection, taskKey);
+    bool goodSection = false;
+
     while(fgets(slowo, sizeof slowo, iniFile)!=NULL)
     {
-        printf("%s",slowo);
         switch (slowo[0])
         {
-        //comment
-        case ';':
-            printf("comment \n");
-            break;
         //new section
         case '[':
-            printf("section \n");
+            
+            if(!strcmp(slowo, taskSection))
+            {
+                goodSection = true;
+            }
+            else
+            {
+                goodSection = false;
+            }
+
             break;
         //key-value
         case 'a' ... 'z':
-            printf("key-value \n");
+            if(goodSection)
+            {
+                strcpy(delim," = ");
+                ptr = strtok(slowo,delim);
+                char key[30];
+                strcpy(key,ptr);
+
+                ptr = strtok(NULL,delim);
+                char value[30];
+                strcpy(value,ptr);
+                if(!strcmp(key,taskKey))
+                {
+                    printf("%s", value);
+                }
+            }
+            
             break;
-        //whitespace
+        //comment and whitespace
+        case ';':
         case '\n':
-            printf("whitespace \n");
             break;
         default:
-            printf("din not recognise \n");
+            printf("input not recognised: %s",slowo);
             break;
         }
     }
